7b80f8cedaaa856b71daae50a326ad36fe60e9eb
BrandonHilligenn <BrandonHilligenn@THWJHBLPT001>
2022-11-02 10:19:57 +0200

    Add project files.

---
+++ AntiApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AntiApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
